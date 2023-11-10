2d23ea9ad35ab65b45af89c903846597cb111cbc
vicky <vicky@Vicky>
2023-11-10 19:37:35 +0530

    First Push

---
+++ Sample/src/com/hcl/pack/SimpleProgram.java
@@ -0,0 +1,18 @@
+package com.hcl.pack;
+
+import java.util.Scanner;
+
+public class SimpleProgram {
+
+
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello DevOps");
+		
+
+	}
+
+
+
+}
